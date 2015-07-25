632d3d600b5e1dfc001e4339817f071da503ee63
robicai <robicai@gmail.com>
2015-07-25 20:11:18 +0800

    chye

--- MenuExamples/app/src/main/java/com/example/robbie/menuexamples/MyDialogFragment.java
+++ MenuExamples/app/src/main/java/com/example/robbie/menuexamples/MyDialogFragment.java
@@ -1,8 +1,22 @@
 package com.example.robbie.menuexamples;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.os.Bundle;
+
 /**
  * Created by robbie on 25/7/2015.
  */
 public class MyDialogFragment extends DialogFragment{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder theDialog = new AlertDialog.Builder(getActivity());
+
+        theDialog.setTitle("Sample Dialog");
+
+        return super.onCreateDialog(savedInstanceState);
+    }
 }
